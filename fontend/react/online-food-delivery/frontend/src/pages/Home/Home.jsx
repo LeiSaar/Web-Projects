246f22e50d1a35ec9ae28cff56409f92747d3b83
Lei <s8lehuan@stud.uni-saarland.de>
2025-11-25 19:20:11 +0100

    futher part for home page

--- fontend/react/online-food-delivery/frontend/src/pages/Home/Home.jsx
+++ fontend/react/online-food-delivery/frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { Header } from '../../komponents/Header/Header';
 import ExploreMenu from '../../komponents/ExploreMenu/ExploreMenu';
 import { FoodDisplay } from '../../komponents/FoodDisplay/FoodDisplay';
+import AppDownload from '../../komponents/AppDownload/AppDownload';
 
 const Home = () => {
   const [category, setCategory] = useState('All');
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category = {category} setCategory = {setCategory}/>
       <FoodDisplay category = {category} />
+      <AppDownload/>
     </div>
   )
 }
