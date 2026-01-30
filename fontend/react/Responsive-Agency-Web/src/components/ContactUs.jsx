dc9ba24daaf43aaa18c89e8e32dec013b7119658
Lei <s8lehuan@stud.uni-saarland.de>
2026-01-30 21:49:55 +0100

    finishing the project with animations

--- fontend/react/Responsive-Agency-Web/src/components/ContactUs.jsx
+++ fontend/react/Responsive-Agency-Web/src/components/ContactUs.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast';
+import { motion } from "motion/react";
 
 const ContactUs = () => {
 
-    const onSubmit = async(event) =>{
+    const onSubmit = async (event) => {
         event.preventDefault();
 
         const formData = new FormData(event.target);
@@ -13,63 +14,74 @@ const ContactUs = () => {
         formData.append("access_key", "017d5476-e746-48ca-831e-4786241e7e96");
 
         try {
-            const response = await fetch ("https://api.web3forms.com/submit", {
-            method:"POST",
-            body: formData
-        });
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            });
 
-        const data = await response.json();
+            const data = await response.json();
 
-        if(data.success){
-            toast.success('Thank you for your submission!');
-            event.target.reset();
-        } else{
-            toast.error(data.message); 
-        }
+            if (data.success) {
+                toast.success('Thank you for your submission!');
+                event.target.reset();
+            } else {
+                toast.error(data.message);
+            }
         } catch (error) {
-             toast.error(error.message); 
-        }  
+            toast.error(error.message);
+        }
     };
 
     return (
-        <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ staggerChildren: 0.2 }}
+            id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40
         pt-30 text-gray-700 dark:text-white'>
 
             <Title title="Reach out to us"
                 desc="From strategy to execution, we craft digital solutions that move your business forward"
             />
 
-            <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+            <motion.form
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                viewport={{ once: true }}
+                onSubmit={onSubmit} 
+                className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
 
-                <div>
+                <motion.div>
                     <p className='mb-2 text-sm font-medium'>Your Name</p>
                     <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
                         <img src={assets.person_icon} alt="" />
-                        <input name='name' type="text" placeholder='Enter your name' className = "w-full p-3 text-sm outline-none" required/>
+                        <input name='name' type="text" placeholder='Enter your name' className="w-full p-3 text-sm outline-none" required />
                     </div>
-                </div>
-                 
-                 <div>
+                </motion.div>
+
+                <motion.div>
                     <p className='mb-2 text-sm font-medium'>Email Id</p>
                     <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
                         <img src={assets.email_icon} alt="" />
-                        <input name="email" type="email" placeholder='Enter your email' className = "w-full p-3 text-sm outline-none" required/>
+                        <input name="email" type="email" placeholder='Enter your email' className="w-full p-3 text-sm outline-none" required />
                     </div>
-                </div>
+                </motion.div>
 
-                <div className='sm:col-span-2'>
+                <motion.div className='sm:col-span-2'>
                     <p className='mb-2 text-sm font-medium'>Message</p>
                     <textarea name="message" rows={8} placeholder='Enter your message' required className='border border-gray-300 dark:border-gray-600 w-full p-3 text-sm outline-none rounded-lg'></textarea>
-                </div>
+                </motion.div>
 
-                <button type="submit" className='bg-primary w-max flex gap-2 text-white text-sm px-10 py-3 rounded-full
+                <motion.button type="submit" className='bg-primary w-max flex gap-2 text-white text-sm px-10 py-3 rounded-full
                 cursor-pointer hover:scale-103 transition-all'>
-                    Submit <img src={assets.arrow_icon} className='w-4' alt=""/>
-                </button>
+                    Submit <img src={assets.arrow_icon} className='w-4' alt="" />
+                </motion.button>
 
-            </form>
+            </motion.form>
 
-        </div>
+        </motion.div>
     )
 }
 
--- fontend/react/Responsive-Agency-Web/src/components/Services.jsx
+++ fontend/react/Responsive-Agency-Web/src/components/Services.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import assets from "../assets/assets";
-import Title from "./Title"
+import Title from "./Title";
 import ServiceCard from "./ServiceCard";
+import {motion} from "motion/react";
+
 
 const Services = () => {
 
@@ -30,7 +32,12 @@ const Services = () => {
     ]
 
     return (
-        <div id = "services" className="relative flex flex-col
+        <motion.div 
+        initial="hidden"
+        whileInView = "visible"
+        viewport={{once:true}}
+        transition={{staggerChildren:0.2}}
+        id = "services" className="relative flex flex-col
         items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 
         text-gray-700 dark:text-white">
               
@@ -44,7 +51,7 @@ const Services = () => {
                     <ServiceCard service = {service} key = {index} index = {index}/>
                 ))}
               </div>
-        </div>
+        </motion.div>
     )
 }
 
